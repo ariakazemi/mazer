111d98e05374ad0120b881855be29b500e84dc3b
Aria <ark.d.night@gmail.com>
2017-01-27 19:10:04 +0330

    Git Added

---
+++ js/smaple.js
@@ -0,0 +1,441 @@
+var sampleString = "\
+using UnityEngine;\n\
+using System.Collections;\n\
+public class sample : MonoBehaviour {\n\
+	public struct block {\n\
+		public int[][] type;\n\
+		public int[][] rotation;\n\
+		public int input;\n\
+		public int output;\n\
+    }\n\
+	public block[][] blocks=new block[3][];\n\
+	public sample(){\n\
+        blocks[0][0].type = new int[2][] {\n\
+            new int[]{2,2,1,3,1},\n\
+            new int[]{2,2,1,3,1}\n\
+        };\n\
+        blocks[0][0].rotation = new int[2][] {\n\
+            new int[]{1,3,0,0,2},\n\
+            new int[]{0,2,1,0,2}\n\
+        };\
+        blocks[0][0].input = 4;\n\
+        blocks[0][0].output = 3;\n\
+    }\n\
+}";
+
+var constructHead = "\
+using UnityEngine;\n\
+using System.Collections;\n\
+public class sample {\n\
+	public struct block {\n\
+		public int[][] type;\n\
+		public int[][] rotation;\n\
+    }\n\
+	public int[,] setInfo=new int[{{blockTypesNumber}},3];\n\
+	public block[,] blocks=new block[{{blockTypesNumber}},{{maxBlocksInSet}}];\n\
+	public int block_i={{blockTypesNumber}},block_j={{maxBlocksInSet}};\n\
+	public sample(){\n";
+var constructBodyType = "		blocks[{{typeIndex}},{{blockIndex}}].type = new int[{{rowNumbers}}][] {\n";
+var constructBodyRow = "			new int[]{";
+var constructEnd = "};\n";
+var constructBodyRotation = "		blocks[{{typeIndex}},{{blockIndex}}].rotation = new int[{{rowNumbers}}][] {\n";
+var blocksTypeInfo = "		setInfo[{{typeIndex}}, 0] = {{input}};\n		setInfo[{{typeIndex}}, 1] = {{output}};\n		setInfo[{{typeIndex}}, 2] = {{blocksCount}};\n\n";
+
+var indexOf = function (input, output) {
+	return 1;
+}
+
+var readBlock = function (block) {
+	var x = block.type[0].length;
+	var y = block.type.length;
+	var entry = block.input;
+	var outry = block.output;
+	//read from block data and put in inputs
+	$("#x").val(x);
+	$("#y").val(y);
+	$("#entry").val(entry);
+	$("#outry").val(outry);
+	scene.refresh();
+	var l = x * y;
+	var modules = $("#scene .module");
+	for (var i = 0; i < y; i++) {
+		//if row doesn't exist add a row
+		for (var j = 0; j < x; j++) {
+			var m = modules[i * x + j];
+			sceneMaker.changeType(m, block.type[i][j]);
+			rotate(m, block.rotation[i][j]);
+		}
+
+	}
+}
+var writeBlock = function (block, data) {
+	if (data) {
+		
+	} else {
+		var x = $("#x").val();
+		var y = $("#y").val();
+		var entry = $("#entry").val();
+		var outry = $("#outry").val();
+
+		var l = x * y;
+		var modules = $("#scene .module");
+		for (var i = 0; i < y; i++) {
+			//if row doesn't exist add a row
+			if (block.type[i] == undefined) {
+				block.type[i] = new Array(parseInt(x));
+				block.rotation[i] = new Array(parseInt(x));
+			}
+			for (var j = 0; j < x; j++) {
+				var m = modules[i * x + j];
+				var r = $(m).attr("data-rotate");
+				if (r == undefined) r = 0;
+				var t = $(m).attr("data-type");
+				block.type[i][j] = t;
+				block.rotation[i][j] = r;
+			}
+		}
+		while (block.type.length > y) {
+			block.type.pop();
+			block.rotation.pop();
+		}
+	}
+
+}
+
+var blockSample = function (set) {
+	return new function () {
+		var block = this;
+		this.index = function () {
+			if (set == undefined) return 0;
+			return block.set.array.indexOf(block);
+		};
+		this.typeIndex = function () {
+			if (set == undefined) return 0;
+			return block.set.index();
+		}
+		this.set = set;
+		this.type = [
+			//row
+			[0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0]
+		];
+		this.rotation = [
+			[0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0]
+		];
+		this.input = 62;
+		this.output = 62;
+
+		if (block.set) {
+			block.input = block.set.input;
+			block.output = block.set.output;
+			var _emptyBlockArray = function (n, m) {
+				var arr = new Array(n);
+				for (var i = 0; i < n; i++) {
+					arr[i] = new Array(m);
+					for (var j = 0; j < m; j++) {
+						arr[i][j] = 0;
+					}
+				}
+				return arr;
+			}
+			block.type = _emptyBlockArray(2, block.set.x);
+			block.rotation = _emptyBlockArray(2, block.set.x);
+		}
+		this.code = function (typeIndex, blockIndex) {
+			/*
+			blocks[0,0].type = new int[2][] {
+				new int[]{2,2,1,3,1},
+				new int[]{2,2,1,3,1}
+			};\
+			blocks[0,0].rotation = new int[2][] {
+				new int[]{1,3,0,0,2},
+				new int[]{0,2,1,0,2}
+			};\
+			*/
+			var codeBlock = "";
+			if (typeIndex == undefined) {
+				var typeIndex = block.typeIndex();
+			}
+			if (blockIndex == undefined) {
+				var blockIndex = block.index();
+			}
+
+			codeBlock = S(constructBodyType).template({
+				typeIndex: typeIndex,
+				blockIndex: blockIndex,
+				rowNumbers: block.type.length
+			}).s;
+			for (var i = 0; i < block.type.length; i++) {
+				codeBlock += constructBodyRow + block.type[i].join(',') + '}';
+				if (i != block.type.length - 1) codeBlock += ',\n';
+				else codeBlock += '\n';
+			}
+			codeBlock += '		' + constructEnd;
+			codeBlock += S(constructBodyRotation).template({
+				typeIndex: typeIndex,
+				blockIndex: blockIndex,
+				rowNumbers: block.type.length
+			}).s;
+			for (var i = 0; i < block.type.length; i++) {
+				codeBlock += constructBodyRow + block.rotation[i].join(',') + '}';
+				if (i != block.type.length - 1) codeBlock += ',\n';
+				else codeBlock += '\n';
+			}
+			codeBlock += '		' + constructEnd;
+			return codeBlock;
+		}
+		this.read = function () {
+			readBlock(block);
+		}
+		this.write = function (data) {
+			writeBlock(block, data);
+		}
+		this.ui = {
+			el: undefined,
+			make: function () {
+				var tmp = $(".blockThumb.template").clone().removeClass("template");
+				tmp[0].block = block;
+				block.set.ui.el.find(".sections").append(tmp);
+				tmp.click(function () {
+					block.set.select(block);
+				});
+				block.ui.el = tmp;
+			}
+		}
+		this.remove = function () {
+			if (window._selectedBlock == block) {
+				block.set.clearSelection();
+				sceneMaker.build();
+			}
+			block.set.removeBlock(block);
+			block.ui.el.remove();
+		}
+	}
+}
+var blocksSample = [
+	//block type set
+	[
+		//block type
+		{
+			type: [
+				//row
+				[0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0]
+			],
+			rotation: [
+				[0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0]
+			],
+			input: 62,
+			output: 62,
+			code: function () {
+
+			}
+		}
+	]
+]
+var blocksTree = [
+	//block type set
+	[
+		//block type
+		{
+			type: [
+				//row
+				[0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0]
+			],
+			rotation: [
+				[0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0]
+			],
+			input: 62,
+			output: 62
+		}
+	]
+]
+
+var construction = {
+	parts: {
+
+	},
+	head: function () {
+		return "";
+	},
+	types: {
+		get: function () {
+			return "blockType";
+		},
+		set: function (number) {
+			var string = S(construction.head()).template({
+				blocksTypeNumber: number
+			}).s;
+
+		}
+	}
+}
+
+var blockSet = function (arr, input, output, x, type) {
+	var splitter = ',';
+	var that = this;
+	this.array = [];
+	this.length = function () {
+		return that.array.length;
+	};
+	if (arr != undefined && arr.length) {
+		this.array = arr;
+	}
+	this.input = input;
+	this.output = output;
+	this.x = x;
+	if (x == undefined) {
+		this.x = 5;
+	}
+	if (type == undefined) {
+		this.type = this.input + splitter + this.output;
+	} else {
+		this.input = this.type.split(splitter)[0];
+		this.output = this.type.split(splitter)[1];
+	}
+	this.index = function () {
+		return blocks.sets.indexOf(that);
+	}
+	this.add = function (block) {
+
+	}
+	this.makeBlock = function (data) {
+		var block = blockSample(that);
+		block.write(data);
+		that.array.push(block);
+		that.ui.el.find(".number-ind").text(that.array.length);
+		blocks.updateBlocksNumber(that.array.length);
+		block.ui.make();
+		return block;
+	}
+	this.code = function () {
+		var typeIndex = that.index();
+		var result = "";
+		result += S(blocksTypeInfo).template({
+				typeIndex: typeIndex,
+				input: that.input,
+				output: that.output,
+				blocksCount:  that.length()
+			}).s;
+		for (var i = 0; i < that.length(); i++) {
+			
+			result += that.array[i].code(typeIndex, i);
+		}
+		return result;
+	}
+	this.selectedBlock = undefined;
+	this.select = function (block, read) {
+
+		if (window._selectedBlock != undefined) {
+			window._selectedBlock.set.clearSelection();
+		}
+		if (typeof block == "number") {
+			//select by index
+			window._selectedBlock = that.array[block];
+		} else {
+			if (that.array.indexOf(block) != -1) {
+				window._selectedBlock = block;
+			} else {
+				throw "can't select a block that's not in set";
+			}
+		}
+		if (read !== false) {
+			window._selectedBlock.read();
+		}
+		window._selectedBlock.ui.el.addClass("selected");
+		that.selectedBlock = block;
+	}
+	this.clearSelection = function () {
+		window._selectedBlock.ui.el.removeClass("selected");
+		window._selectedBlock = undefined;
+		that.selectedBlock = undefined;
+	}
+	this.ui = {
+		el: undefined,
+		make: function () {
+			var setUi = $(".wayGroup li[data-type='TT']".replace("TT", that.type));
+			if (setUi.length == 0) {
+				setUi = $(".set.template").clone().removeClass("template").appendTo(".wayGroup").attr("data-type", that.type);
+
+				var input = parseInt(that.input).toString(2);
+				var output = parseInt(that.output).toString(2);
+
+				while (input.length < that.x) input = '0' + input;
+				while (output.length < that.x) output = '0' + output;
+				for (var i = 0; i < that.x; i++) {
+					var ou = "",
+						en = "";
+					if (input[i] == 1) en = "on";
+					if (output[i] == 1) ou = "on";
+					setUi.find(".input-inds").append($("<div>").addClass("input-ind").addClass(en));
+					setUi.find(".output-inds").append($("<div>").addClass("output-ind").addClass(ou));
+				}
+			}
+			that.ui.el = setUi;
+
+			return setUi;
+		}
+
+	}
+	that.ui.make();
+	this.removeBlock = function (block) {
+		that.array.splice(that.array.indexOf(block), 1);
+		that.ui.el.find(".number-ind").text(that.array.length);
+		blocks.updateBlocksNumber(that.array.length);
+	}
+}
+var blocks = {
+	exportCode: function () {
+		var result = S(constructHead).template({
+			blockTypesNumber: blocks.sets.length,
+			maxBlocksInSet: blocks.maxBlocks
+		}).s;
+
+		for (var i = 0; i < blocks.sets.length; i++) {
+			result += blocks.sets[i].code();
+		}
+		return result + '	' + constructEnd + constructEnd;
+	},
+	sets: [],
+	typeMap: {},
+	makeSet: function (entry, outry) {
+		if (arguments.length == 0) {
+			var entry = $("#entry").val();
+			var outry = $("#outry").val();
+		}
+		var type = blocks.makeType(entry, outry);
+		var set = new blockSet([], entry, outry);
+		blocks.sets.push(set);
+		blocks.typeMap[type] = set;
+		return set;
+	},
+	getSet: function (entry, outry) {
+		var set = blocks.typeMap[blocks.makeType(entry, outry)]
+		if (set) {
+			return set;
+		} else return false;
+	},
+	makeType: function (entry, outry) {
+		var splitter = ',';
+
+		var type = entry + splitter + outry;
+		return type;
+
+	},
+	clear: function () {
+		for (var i = 0;i < blocks.sets.length; i++) {
+			blocks.sets[i].ui.el.remove();
+		}
+		blocks.sets = [];
+	},
+	updateBlocksNumber: function (num) {
+		if (blocks.maxBlocks < num) {
+			blocks.maxBlocks = num;
+		}
+	},
+	maxBlocks: 0
+}
---
+++ script.js
@@ -0,0 +1,411 @@
+var sceneMaker = {
+	build: function () {
+		var x = $("#x").val();
+		var y = $("#y").val();
+		var entry = $("#entry").val();
+		var outry = $("#outry").val();
+		for (var i = 1; i < 10; i++) {
+			$("#scene").removeClass("cols-" + i);
+		}
+		$("#scene").addClass("cols-" + x);
+
+		if (x == "") {
+			x = 5;
+			$("#x").val(5);
+		}
+		if (y == "") {
+			y = 2;
+			$("#y").val(2);
+		}
+
+		var modules = $("#scene .module");
+		var dif = x * y - modules.length;
+		if (dif > 0) {
+			for (; dif > 0; dif--) {
+				moduleClickFunc($(".module.template").clone().removeClass("template").appendTo("#scene"));
+			}
+		} else if (dif < 0) {
+			for (; dif < 0; dif++) {
+				$(modules[modules.length - 1]).remove();
+				var modules = $("#scene .module");
+			}
+		}
+
+		sceneMaker.type.sync();
+	},
+	type: {
+		get: function () {
+			var x = $("#x").val();
+			var first5 = $("#scene .module").slice(0, x);
+			var last5 = $("#scene .module").slice(-x);
+			var entry = '';
+			var outry = ''
+			first5.each(function () {
+				if (sceneMaker.type.canExit(this)) {
+					outry += '1';
+				} else {
+					outry += '0';
+				}
+			});
+			last5.each(function () {
+				if (sceneMaker.type.canEnter(this)) {
+					entry += '1';
+				} else {
+					entry += '0';
+				}
+			});
+			return [entry, outry];
+		},
+		set: function (entry, outry) {
+			try {
+				$("#entry").val(parseInt(entry, 2));
+			} catch (e) {
+				//do nothing
+			}
+			try {
+				$("#outry").val(parseInt(outry, 2));
+			} catch (e) {
+				//do nothing
+			}
+		},
+		sync: function () {
+			var data = sceneMaker.type.get();
+			sceneMaker.type.set(data[0], data[1]);
+		},
+		canEnter: function (module) {
+			var type, rotation;
+			type = $(module).attr("data-type").slice(-1);
+			rotation = $(module).attr("data-rotate");
+			if (type == 0) return true;
+			else if (type == 1 && rotation != 3) return true;
+			else if (type == 2 && rotation % 2 != 0) return true;
+			else if (type == 3 && rotation < 2) return true;
+			else if (type == 4 && rotation == 1) return true;
+			else return false;
+		},
+		canExit: function (module) {
+			var type, rotation;
+			type = $(module).attr("data-type").slice(-1);
+			rotation = $(module).attr("data-rotate");
+			if (type == 0) return true;
+			else if (type == 1 && rotation != 1) return true;
+			else if (type == 2 && rotation % 2 != 0) return true;
+			else if (type == 3 && rotation > 1) return true;
+			else if (type == 4 && rotation == 3) return true;
+			else return false;
+		}
+	},
+	select: function (module) {
+		$(module).addClass("selected");
+		if ($("#scene").hasClass("active")) {
+
+		} else {
+			sceneMaker.activate();
+
+		}
+	},
+	deselect: function (module) {
+		$(module).removeClass("selected");
+	},
+	activate: function () {
+		$("#scene").addClass("active");
+		$("footer").addClass("active");
+		sceneMaker.attachEvents();
+	},
+	deactivate: function () {
+		$("#scene").removeClass("active");
+		$("footer").removeClass("active");
+		sceneMaker.detachEvents();
+	},
+	changeType: function (module, type) {
+		$(module).attr("data-type", type);
+		$(module).trigger("alter");
+	},
+	move: function (direction) {
+
+	},
+	addRow: function () {
+		$("#y").val(parseInt($("#y").val()) + 1);
+		scene.refresh();	
+	},
+	removeRow: function () {
+		$("#y").val(parseInt($("#y").val()) - 1);
+		scene.refresh();	
+	},
+	attachEvents: function () {
+		var eventHandler = function (e) {
+			var module = $("#scene .module.selected");
+			if (e.keyCode == 49) {
+				sceneMaker.changeType(module, 0);
+			} else if (e.keyCode == 50) {
+				sceneMaker.changeType(module, 1);
+			} else if (e.keyCode == 51) {
+				sceneMaker.changeType(module, 2);
+			} else if (e.keyCode == 52) {
+				sceneMaker.changeType(module, 3);
+			} else if (e.keyCode == 53) {
+				sceneMaker.changeType(module, 4);
+			} else if (e.keyCode == 82) {
+				rotate(module);
+			} else if (e.keyCode == 39) {
+				if (module.next().length) {
+					sceneMaker.select(module.next());
+					sceneMaker.deselect(module);
+				}
+
+			} else if (e.keyCode == 37) {
+				if (module.prev().length) {
+					sceneMaker.select(module.prev());
+					sceneMaker.deselect(module);
+				}
+			} else if (e.keyCode == 38) {
+				var x = $("#x").val();
+				var possibles = module.prevAll();
+				if (possibles.length >= x) {
+					sceneMaker.deselect(module);
+					sceneMaker.select(possibles[x - 1]);
+				}
+			} else if (e.keyCode == 40) {
+				var x = $("#x").val();
+				var possibles = module.nextAll();
+				if (possibles.length >= x) {
+					sceneMaker.deselect(module);
+					sceneMaker.select(possibles[x - 1]);
+				} else {
+					$("#y").val(parseInt($("#y").val()) + 1);
+					scene.refresh();
+					possibles = module.nextAll();
+					sceneMaker.deselect(module);
+					sceneMaker.select(possibles[x - 1]);
+				}
+			} else if (e.keyCode == 187) {
+				$("#y").val(parseInt($("#y").val()) + 1);
+				scene.refresh();
+			} else if (e.keyCode == 189) {
+				$("#y").val(parseInt($("#y").val()) - 1);
+				scene.refresh();
+			} else if (e.keyCode == 219) {
+				$("#x").val(parseInt($("#x").val()) - 1);
+				scene.refresh();
+			} else if (e.keyCode == 221) {
+				$("#x").val(parseInt($("#x").val()) + 1);
+				scene.refresh();
+			}
+
+
+		}
+		$("body").on("keydown", eventHandler);
+		$("body")[0].detachKeyboardControls = function () {
+			$("body").off("keydown", eventHandler)
+		}
+	},
+	detachEvents: function () {
+		$("body")[0].detachKeyboardControls();
+	}
+}
+var scene = {
+	refresh: function () {
+		sceneMaker.build();
+	},
+	save: function (makeNew) {
+		var set, block;
+		//does set exist ?
+		var entry = $("#entry").val();
+		var outry = $("#outry").val();
+		set = blocks.getSet(entry, outry);
+		//yes
+		if (set) {
+			block = set.selectedBlock;
+		}
+		//no
+		else {
+			var set = blocks.makeSet();
+			if (window._selectedBlock != undefined && makeNew != true) {
+				window._selectedBlock.remove();
+			}
+			block = false;
+		}
+
+		//does block exist? 
+		//yes
+		if (block && makeNew != true) {
+			block.write();
+		} else {
+			if (window._selectedBlock != undefined && makeNew != true) {
+				window._selectedBlock.remove();
+			}
+			var b = set.makeBlock();
+			set.select(b, false);
+		}
+	},
+	readAll: function (clean) {
+		if (clean) {
+			blocks.clear();
+		}
+		
+		var all = S(allCode.getValue());
+		var blocksData = all.between("sample(){", "};\n};");
+		var blockSetNumber = all.between("public block[,] blocks=new block[",",").s;
+		
+		var blocksDataArray = blocksData.split(';');
+		var blockTypeData,
+			blockRotateData,
+			setInputData,
+			setOutputData,
+			setBlocksNumber,
+			blockSet,
+			setInput,
+			setOutput,
+			setType,
+			setNumber,
+			typeArrays,rotateArrays,y,x;
+		
+		setNumber = S(blocksData).count("setInfo") / 3;
+		var blocksNumber = parseInt((blocksDataArray.length - setNumber * 3) / 2);
+		var setStartIndex = 0;
+		for (var i = 0; i < setNumber; i++) {
+			var blocksStartIndex = setStartIndex + 3;
+			setInputData = blocksDataArray[setStartIndex];
+			setOutputData = blocksDataArray[setStartIndex + 1];
+			setBlocksNumber = blocksDataArray[setStartIndex + 2];
+			
+			setInput = parseInt(setInputData.split('=')[1]);
+			setOutput = parseInt(setOutputData.split('=')[1]);
+			setBlocksNumber = parseInt(setBlocksNumber.split('=')[1]);
+			
+			set = blocks.getSet(setInput, setOutput);
+			if (set == false) {
+				set = blocks.makeSet(setInput, setOutput);
+			}
+			
+			for (var j = 0; j < setBlocksNumber;j++) {
+				var k = blocksStartIndex + j *2;
+				blockTypeData = blocksDataArray[k];
+				blockRotateData = blocksDataArray[k+1];
+				
+				block = set.makeBlock();
+				
+				typeArrays = blockTypeData.split("new");
+				typeArrays.splice(0,2);
+				y = typeArrays.length;
+				for (var j = 0;j < y;j++) {
+					typeArrays[j] = S(typeArrays[j]).between('{','}').s.split(',');
+				}
+				rotateArrays = blockRotateData.split("new");
+				rotateArrays.splice(0,2);
+				for (var j = 0;j < y;j++) {
+					rotateArrays[j] = S(rotateArrays[j]).between('{','}').s.split(',');
+				}
+				block.type = typeArrays;
+				block.rotation = rotateArrays;
+			}
+			setStartIndex += 3 + setBlocksNumber * 2;
+		}
+		
+	},
+	writeAll: function () {
+		allCode.setValue(blocks.exportCode());
+	},
+	changeFamily: function (select) {
+		var current = $("#scene").data("family");
+		var next = $(select).val();
+		$("#scene").removeClass(current).addClass(next).data("family", next);
+	},
+	readFile: function (fileInput) {
+		var reader = new FileReader();
+		var file = fileInput.files[0];
+		if (file) {
+			reader.onload = (function (reader) {
+				return function () {
+					var contents = reader.result;
+					allCode.setValue(contents);
+				}
+			})(reader);
+			reader.readAsText(file);
+			fileInput.form.reset();
+		}
+		
+		
+	}
+}
+var rotate = function (module, i) {
+
+	if (i && i >= 0 && i < 4) {
+		$(module).attr("data-rotate", i);
+	} else {
+		var currentRotation = $(module).attr("data-rotate");
+		if (currentRotation == undefined) {
+			var currentRotation = 0;
+		}
+		currentRotation++;
+		if (currentRotation == 4) {
+			currentRotation = 0;
+		}
+		$(module).attr("data-rotate", currentRotation);
+	}
+	$(module).trigger("alter");
+}
+var moduleClickFunc = function (selector) {
+	if (selector == undefined) {
+		var selector = $("#scene .module");
+	}
+	selector.on("click", function () {
+		if ($(this).hasClass("selected")) {
+			sceneMaker.deselect(this);
+			sceneMaker.deactivate();
+		} else {
+			sceneMaker.deselect($("#scene .module.selected"));
+			sceneMaker.select(this);
+		}
+
+	});
+}
+var downloadAll = function () {
+
+	var file = new File([allCode.getValue()], "sample.cs", {
+		type: "text/plain;charset=utf-8"
+	});
+	saveAs(file);
+}
+$(function () {
+	moduleClickFunc();
+	$(".button-collapse").sideNav({
+		edge: 'right'
+	});
+	$(".moduleType").on("click", function () {
+		var type = $(this).data("type");
+		sceneMaker.changeType($("#scene .module.selected")[0], parseInt(type[type.length - 1]));
+	});
+	$('select').material_select();
+	$('.collapsible').collapsible();
+	$("body").on("keydown", function (e) {
+		if (e.keyCode == 13) {
+			var module = $("#scene .module.selected");
+			if (module.length) {
+				sceneMaker.deselect(module);
+				sceneMaker.deactivate();
+			} else {
+				sceneMaker.select($("#scene .module")[0]);
+			}
+		}
+	});
+	$("section.toolbar, aside").on("keydown", function (e) {
+		e.stopPropagation();
+	});
+	window.allCode = CodeMirror.fromTextArea($("#code-text")[0], {
+		lineNumbers: true,
+		mode: "text/x-csharp",
+		theme: "material"
+	});
+	sceneMaker.build();
+
+	$("#scene").on("alter", ".module", function () {
+		var x = $("#x").val();
+		var l = $("#scene .module").length;
+		var i = $("#scene .module").index(this);
+		if (i < x || i > l - x - 1) {
+			scene.refresh()
+		}
+	});
+
+});
